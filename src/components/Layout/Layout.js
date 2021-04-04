388867e5e8a8c4e374d1ade82b3658f1b1bd1d45
arfaz26 <chougulearfaz@gmail.com>
2021-04-04 21:29:58 +0530

    burger created and handlers

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import classes from "./Layout.module.css";
 
 const layout = (props) => {
   return (
     <Aux>
       <div>placeholder </div>
-      <main>{props.children}</main>
+      <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
 };
