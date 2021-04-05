d0f51be316d88a52b01832abf3d13c9d7e6b3097
arfaz26 <chougulearfaz@gmail.com>
2021-04-05 10:54:33 +0530

    toolbar added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
+import Toolbar from "../Burger/Navigation/Toolbar";
 
 const layout = (props) => {
   return (
     <Aux>
-      <div>placeholder </div>
+      <Toolbar />
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
