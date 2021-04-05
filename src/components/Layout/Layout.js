a63ee824b656e86fbf838bccaa0f414139b6ddb9
arfaz26 <chougulearfaz@gmail.com>
2021-04-05 14:20:14 +0530

    sidedrawer implemented

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Burger/Navigation/Toolbar";
+import SideDrawer from "../Burger/Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => {
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
