2846de62ec34caf26a3e059bdc0ffad07e2de5ec
arfaz26 <chougulearfaz@gmail.com>
2021-04-16 14:21:12 +0530

    test cases written

---
+++ src/components/Burger/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+configure({ adapter: new Adapter() });
+
+describe("<NavigationItems/>", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+  it("should render two navigationitem if not authenticated", () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render 3 navigationitem if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+});
